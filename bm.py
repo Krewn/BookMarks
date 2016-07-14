e927f7e1674dd0c074a135e5f07bfeb7813773af
Kevin Nelson <Kevin Nelson>
2016-07-13 21:04:56 -0400

    init

---
+++ bm.py
@@ -0,0 +1,23 @@
+import sys
+args = sys.argv
+import os
+os.chdir("/home/kevin/bm")
+
+LinkTitlePairs = open("saved.pyDat","a")
+
+if len(args)==2:
+	if "http" in args[1]:
+		LinkTitlePairs.write("\n"+str([args[1],args[2]]))
+	else:
+		print "please include full web adress."
+
+if len(args)==3:
+	if("http" in args[2]):
+		LinkTitlePairs.write("\n"+str([args[1],args[2]]))
+	elif("http" in args[1]):
+		LinkTitlePairs.write("\n"+str([args[2],args[1]]))
+	else:
+		print "please include full web adress."
+
+LinkTitlePairs.close()
+
